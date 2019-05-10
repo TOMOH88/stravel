5538588dff5644b6fc525378d87276d81025e165
TOMOH88 <tomoh88.wiki@gmail.com>
2019-05-10 15:53:14 +0900

    오류수정

--- src/main/java/com/travelmaker/stravel/calendar/controller/CalendarController.java
+++ src/main/java/com/travelmaker/stravel/calendar/controller/CalendarController.java
@@ -65,7 +65,7 @@ public class CalendarController {
 		return "calendar/calendarViewList";
 	}*/
 	
-	@RequestMapping("qnalist.do")
+/*	@RequestMapping("qnalist.do")
 	public String moveFaqListPage() {
 		return "qna/qnaList";
 	}
@@ -83,7 +83,7 @@ public class CalendarController {
 	@RequestMapping("faqlist3.do")
 	public String moveQnaList3Page() {
 		return "faq/faqList3";
-	}
+	}*/
 	
 }
 
